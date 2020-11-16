ffd6ee1ebc1774626ec8b8c8cb353a09416cea49
gberto <gberto@fbk.eu>
2020-11-16 15:15:12 -0500

    update

--- experiments-BL/download_fa.sh
+++ experiments-BL/download_fa.sh
@@ -7,32 +7,32 @@
 projID=5fa925f3afcdcce87fc3facc
 
 out_dir=../data/HCP-anat/images
-json_fname=dataset38fa.json
 datatype=neuro/tensor
 tag=t1_warping_mni152
 
 echo "Downloading FAs..."
 
 #cache the list of datasets that we could download
-if [ ! -f $json_fname ]; then
-    bl dataset query --limit 10000 -p $projID --datatype $datatype --tag $tag --json > $json_fname
+if [ ! -f all.json ]; then
+    bl dataset query --limit 10000 -p $projID --datatype $datatype --tag $tag --json > all.json
 fi
 
 n_sub=0
 
-for subjID in $(jq -r '.[].meta.subject' $json_fname | sort -u); do
+for subjID in $(jq -r '.[].meta.subject' all.json | sort -u); do
 
 	echo "Subject:$subjID"
-    ids=$(jq -r '.[] | select(.meta.subject == '\"$subjID\"') | ._id' $json_fname)
+    ids=$(jq -r '.[] | select(.meta.subject == '\"$subjID\"') | ._id' all.json)
     n_obj=0
 
     for id in $ids
     do
         echo "downloading object:$id"
-        #bl dataset download $id
-        #out_fname=$out_dir/sub-${subjID}_space-mni152_fa.nii.gz
-        #mv $id/fa.nii.gz $out_fname
-        #rm -r $id
+        bl dataset download $id
+        out_fname=$out_dir/sub-${subjID}_space-mni152_fa.nii.gz
+        mv $id/fa.nii.gz $out_fname
+        echo "$out_fname successfully saved."
+        rm -r $id
 
         ((n_obj+=1))
     done
@@ -41,4 +41,5 @@ for subjID in $(jq -r '.[].meta.subject' $json_fname | sort -u); do
     ((n_sub+=1))
 
 done
+rm all.json
 echo "Total number of subjects:$n_sub"
--- experiments-BL/download_t1.sh
+++ experiments-BL/download_t1.sh
@@ -7,33 +7,33 @@
 projID=5fa925f3afcdcce87fc3facc
 
 out_dir=../data/HCP-anat/images
-json_fname=dataset38t1.json
 datatype=neuro/anat/t1w
 datatype_tag=!defaced
 
 echo "Downloading T1s not defaced..."
 
 #cache the list of datasets that we could download
-if [ ! -f $json_fname ]; then
+if [ ! -f all.json ]; then
     echo "Retrieving IDs..."
-    bl dataset query --limit 10000 -p $projID --datatype $datatype --datatype_tag $datatype_tag --json > $json_fname
+    bl dataset query --limit 10000 -p $projID --datatype $datatype --datatype_tag $datatype_tag --json > all.json
 fi
 
 n_sub=0
 
-for subjID in $(jq -r '.[].meta.subject' $json_fname | sort -u); do
+for subjID in $(jq -r '.[].meta.subject' all.json | sort -u); do
 
 	echo "Subject:$subjID"
-    ids=$(jq -r '.[] | select(.meta.subject == '\"$subjID\"') | ._id' $json_fname)
+    ids=$(jq -r '.[] | select(.meta.subject == '\"$subjID\"') | ._id' all.json)
     n_obj=0
 
     for id in $ids
     do
         echo "downloading object:$id"
-        #bl dataset download $id
-        #out_fname=$out_dir/sub-${subjID}_space-mni152_t1.nii.gz
-        #mv $id/t1.nii.gz $out_fname
-        #rm -r $id
+        bl dataset download $id
+        out_fname=$out_dir/sub-${subjID}_space-mni152_t1.nii.gz
+        mv $id/t1.nii.gz $out_fname
+        echo "$out_fname successfully saved."
+        rm -r $id
 
         ((n_obj+=1))
     done
@@ -42,5 +42,5 @@ for subjID in $(jq -r '.[].meta.subject' $json_fname | sort -u); do
     ((n_sub+=1))
 
 done
-rm $json_fname
+rm all.json
 echo "Total number of subjects:$n_sub"
